6d8af99ccab99d8ee81f8cbe1aedfb8f9427db78
Ashish Chodankar <acchodankar@gmail.com>
2022-04-12 22:12:23 +0530

    Navbar changes name and jsx file

--- src/components/NavbarTryItOut.jsx
+++ src/components/NavbarTryItOut.jsx
@@ -84,15 +84,20 @@
 
 // export default Navbar;
 import React from "react";
-import { Search, ShoppingCart } from "@material-ui/icons";
+import { NoEncryption, Search, ShoppingCart } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
 import { useEffect } from "react";
 import styled from "styled-components";
 import { Form, FormControl, Button, Navbar } from "react-bootstrap";
 import Img from "../Images/tryitouttranslogo.png";
-
+import { NavBar, Nav, NavItem, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import { fontSize, sizeHeight } from "@mui/system";
+import "../stylesheets/file.css";
+import { Avatar } from "@mui/material";
+import { NorthEastTwoTone } from "@mui/icons-material";
 
 const Container = styled.div`
   height: 60px;
@@ -149,53 +154,74 @@ const Input = styled.input`
 `;
 const NavbarTryItOut = () => {
   return (
-    <Container className="h-100">
-      <Wrapper>
-        <Left>
-          <img style={{ width: "54px", margin: "2px" }} src={Img}></img>
-          <Logo>TryItOut</Logo>
-        </Left>
-        <Center>
-          <Form className="d-flex" style={{ width: 500 }}>
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
+    <div className="row">
+      <Container className="h-100">
+        <Wrapper>
+          <div className="col-3">
+            <Left>
+              <a href="/home">
+                <img style={{ width: "54px", margin: "2px" }} src={Img}></img>
+              </a>
+              <a href="/home">
+                <Logo style={{ textDecoration: "none" }}>
+                  <Link className="" tag="a" to="/" action></Link>
+                  TryItOut
+                </Logo>
+              </a>
+            </Left>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <div className="col-3">
+              <Center>
+                <Form className="d-flex" style={{ width: 500 }}>
+                  <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-dark">Search</Button>
+                </Form>
+              </Center>
+            </div>
 
-            <Button variant="outline-dark">Search</Button>
-          </Form>
-        </Center>
-        <Right>
-          <MenuItem>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              tag="a"
-              to="/"
-              action
-            >
-              SIGN IN
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              tag="a"
-              to="/"
-              action
-            >
-              LOG OUT
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Badge badgeContent={4} color="primary">
-              <ShoppingCart />
-            </Badge>
-          </MenuItem>
-        </Right>
-      </Wrapper>
-    </Container>
+            <Right>
+              <div className="col-6 row d-flex justify-content-center align-content-center">
+                <div className="col-4"></div>
+                <div className="col-4">
+                  <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                      <ShoppingCart />
+                    </Badge>
+                  </MenuItem>
+                </div>
+                <div className="col-2">
+                  {/* <Avatar src="../Images/arrow.png" /> */}
+                  <Avatar alt="" src={require("../Images/cart.png")} />
+                </div>
+                <div className="col-2">
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      variant="primary-light"
+                      id="dropdown-basic"
+                    ></Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="/user-profile">
+                        User Profile
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/your-orders">
+                        User Orders
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/">Log Out</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+              </div>
+            </Right>
+          </div>
+        </Wrapper>
+      </Container>
+    </div>
   );
 };
 
